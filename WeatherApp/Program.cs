0d9c408709283ed0d33c51691f87717516b2e04b
Joachim Carlsson <carlsson.joachim@gmail.com>
2025-02-11 09:58:50 +0100

    Add project files.

---
+++ WeatherApp/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace WeatherApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Tänkte snacka lite om vädret...");
+        }
+    }
+}
