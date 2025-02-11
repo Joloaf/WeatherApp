261354f8e106498fa25e40ab61057bca2c49416a
Joachim Carlsson <carlsson.joachim@gmail.com>
2025-02-11 12:57:44 +0100

    Removed Models folder for the dababase. We're not using a database

--- WeatherApp/TextToDb.cs
+++ WeatherApp/TextToDb.cs
@@ -13,7 +13,6 @@ namespace WeatherApp
             string pattern = "(?<year>\\d{4})-(?<month>(0[1-9]|1[0-2]))-(?<day>(0[1-9]|[12][0-9]|3[01]))\\s(?<time>\\d{2}:\\d{2}:\\d{2}),(?<plats>\\w+),(?<temp>-?(4[0-9]|50|\\d{1,2})\\.\\d+),(?<luftfuktighet>(0|[1-9][0-9]?|100))\r\n";
 
             string[] lines = File.ReadAllLines(@"..\..\..\Files\tempData_medFel.txt");
-
         }
     }
 }
